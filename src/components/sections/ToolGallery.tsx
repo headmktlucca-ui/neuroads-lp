541d6ffe53af7de55d36fd30f3bc7365c532a4f1
headmktlucca-ui <headmktlucca@gmail.com>
2026-04-09 13:32:10 -0300

    v121

--- src/components/sections/ToolGallery.tsx
+++ src/components/sections/ToolGallery.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
 import TrafficAnalystContainer, { ActiveAppConfig } from '../traffic-analyst/TrafficAnalystContainer';
 
