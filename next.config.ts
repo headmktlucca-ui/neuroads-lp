e8cd4a7428302c7887769f2360eaf9b3e5c307db
headmktlucca-ui <headmktlucca@gmail.com>
2026-04-14 00:41:43 -0300

    987

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["neuroads.com.br", "*.neuroads.com.br"],
+    },
+  },
 };
 
 export default nextConfig;
