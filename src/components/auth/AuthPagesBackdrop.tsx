00a5ae8e12e904650cdc7fce53e3063c7638023b
headmktlucca-ui <headmktlucca@gmail.com>
2026-05-29 19:55:02 -0300

    29-05-2026.01

--- src/components/auth/AuthPagesBackdrop.tsx
+++ src/components/auth/AuthPagesBackdrop.tsx
@@ -4,24 +4,26 @@ import Image from 'next/image';
 
 export function AuthPagesBackdrop() {
   return (
-    <div className="pointer-events-none fixed inset-0 z-0">
-      <div
-        className="absolute inset-0 bg-center bg-cover bg-no-repeat bg-white"
-        style={{ backgroundImage: "url('/images/background_hub_custom_wave.png')" }}
-      />
+    <>
+      <div className="pointer-events-none fixed inset-0 z-0">
+        <div
+          className="absolute inset-0 bg-center bg-cover bg-no-repeat bg-white"
+          style={{ backgroundImage: "url('/images/background_hub_custom_wave.png')" }}
+        />
+      </div>
 
-      <div className="absolute left-4 top-4 sm:left-7 sm:top-6 pointer-events-auto z-10">
-        <a href="https://neuroads.com.br" rel="noopener noreferrer">
+      <div className="pointer-events-auto fixed left-1/2 top-4 -translate-x-1/2 z-20 sm:top-6 flex justify-center">
+        <a href="https://neuroads.com.br" rel="noopener noreferrer" className="block group">
           <Image
-            src="/images/auth/logo-neuroads23-generated.png"
+            src="/images/logo2026.png"
             alt="NeuroAds"
-            width={280}
-            height={76}
+            width={240}
+            height={52}
             priority
-            className="h-auto w-[180px] sm:w-[230px] lg:w-[280px] cursor-pointer hover:opacity-90 transition-opacity"
+            className="h-auto w-[150px] sm:w-[190px] lg:w-[230px] cursor-pointer group-hover:scale-[1.02] transition-all duration-300"
           />
         </a>
       </div>
-    </div>
+    </>
   );
 }
